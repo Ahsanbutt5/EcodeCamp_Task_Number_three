e31cb8a2f9bf94e4d778c80a161f18c7b7e3d339
Ahsan Abdulah Butt <ahsanabdullah132@gmail.com>
2024-09-12 19:21:25 +0500

    first commit

---
+++ lib/model/category.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class Category{
+  final String categoryTitle;
+  final IconData categoryIcon;
+
+  const Category({
+    required this.categoryTitle,
+    required this.categoryIcon
+  });
+}
